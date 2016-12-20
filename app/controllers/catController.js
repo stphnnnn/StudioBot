3b80302fb133117013dd2caa0058e2ccd6fdc4d9
Steve Burtenshaw <steve.burtenshaw@gmail.com>
2016-12-20 10:27:36 +0000

    add all slack emojis

--- app/controllers/catController.js
+++ app/controllers/catController.js
@@ -1,5 +1,5 @@
 var request = require('request');
-
+var catEmojis = [ ':smiley_cat:', ':smile_cat:', ':heart_eyes_cat:', ':kissing_cat:', ':smirk_cat:', ':scream_cat:', ':crying_cat_face:', ':joy_cat:' ]
 function getCat(callback) {
   request('http://thecatapi.com/api/images/get?format=html&type=gif', function (error, response, body) {
     if (!error && response.statusCode == 200) {
@@ -11,7 +11,7 @@ function getCat(callback) {
 }
 
 module.exports = function(controller, visitor) {
-  controller.hears([':cat:', ':cat2:'], 'direct_message', function(bot, message) {
+  controller.hears(catEmojis, 'direct_message', function(bot, message) {
     getCat(function (catUrl) {
       console.log(catUrl);
       var res = {
